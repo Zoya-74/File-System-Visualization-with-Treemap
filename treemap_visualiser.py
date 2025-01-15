f3c76a287809b2963039499fc8401efeac0146c0
Zoya-74 <151096154+Zoya-74@users.noreply.github.com>
2025-01-15 11:57:04 -0500

    Update treemap_visualiser.py

--- treemap_visualiser.py
+++ treemap_visualiser.py
@@ -1,17 +1,4 @@
 """
-Assignment 2: Treemap Visualiser
-
-=== CSC148 Winter 2024 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2024 Bogdan Simion, David Liu, Diane Horton,
-                   Haocheng Hu, Jacqueline Smith
-
 === Module Description ===
 This module contains the main program code for the treemap visualisation.
 It is responsible for initializing an instance of TMTree (using a
